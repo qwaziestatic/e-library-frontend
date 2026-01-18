27886ad617015ed72a7f1fc148a02c4e8b04e1aa
qwaziestatic <139397610+qwaziestatic@users.noreply.github.com>
2026-01-18 04:03:59 +0300

    Update axios.js

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
 
 const api = axios.create({
-  // Ensure your Vercel Environment Variable includes the "/api" at the end
   baseURL: import.meta.env.VITE_API_BASE_URL || 'http://localhost:3000/api',
 });
 
